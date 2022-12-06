9e27443a4c63a87e993b29b2a12dddc9b722fe97
IAbulizi <izdar.abulizi@outlook.com>
2022-12-06 10:11:48 +0100

    Add verbose flag to read_file()

--- lib.py
+++ lib.py
@@ -1,5 +1,6 @@
-def read_file(filename):
+def read_file(filename, verbose = False):
     with open(filename) as file:
         lines = [line.rstrip() for line in file]
-    print(f"Read file, print first 5 lines:\n{lines[:10]}")
+    if verbose:
+        print(f"Read file, print first 5 lines:\n{lines[:10]}")
     return lines
