47db211c30c191d0d5502c1f1eb40f4a3ef5feec
IAbulizi <izdar.abulizi@outlook.com>
2022-12-08 23:19:46 +0100

    Modify requirement, add smart maximum function

--- lib.py
+++ lib.py
@@ -1,6 +1,14 @@
+import numpy as np
+
 def read_file(filename, verbose = False):
     with open(filename) as file:
         lines = [line.rstrip() for line in file]
     if verbose:
         print(f"Read file, print first 5 lines:\n{lines[:10]}")
     return lines
+
+def get_max_day8(arr):
+    try:
+        return np.max(arr)
+    except ValueError:
+        return -1
