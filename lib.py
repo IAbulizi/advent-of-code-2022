920dc7ec3139a3d0534ce15d2c5ab33e0dc0dea8
IAbulizi <izdar.abulizi@outlook.com>
2022-12-12 01:04:13 +0100

    Add stub for day 11

--- lib.py
+++ lib.py
@@ -1,11 +1,16 @@
 import numpy as np
 
-def read_file(filename, verbose = False):
+def read_file(filename : str, verbose = False):
     with open(filename) as file:
         lines = [line.rstrip() for line in file]
     if verbose:
         print(f"Read file, print first 5 lines:\n{lines[:10]}")
     return lines
+
+def read_file_as_one(filename : str) -> str:
+    with open(filename) as file:
+        input = file.read()
+    return input
 
 def get_max_day8(arr):
     try:
